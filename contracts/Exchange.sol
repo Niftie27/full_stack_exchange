087774a3d3aadff77293e32beb0161cac1c1fccd
tomas <tom.pazout@gmail.com>
2025-03-12 19:58:32 +0100

    10. Withdraw tokens

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -12,6 +12,12 @@ contract Exchange {
 	mapping(address => mapping(address => uint256)) public tokens;
 
 	event Deposit(address token, address user, uint256 amount, uint256 balance);
+	event Withdraw(
+		address token, 
+		address user, 
+		uint256 amount, 
+		uint256 balance
+	);
 
 	constructor(address _feeAccount, uint256 _feePercent) {
 		feeAccount = _feeAccount;
@@ -22,7 +28,7 @@ contract Exchange {
 	// DEPOSIT AND WITHDRAW TOKEN
 
 	function depositToken(address _token, uint256 _amount) public {
-		//Transfer tokens to exchange
+		// Transfer tokens to exchange
 		require(Token(_token).transferFrom(msg.sender, address(this), _amount));
 
 		// Update user balance
@@ -30,6 +36,22 @@ contract Exchange {
 
 		// Emit and event
 		emit Deposit(_token, msg.sender, _amount, tokens[_token][msg.sender]);
+	}
+
+	function withdrawToken(address _token, uint256 _amount) public {
+		// Ensure user has enough tokens to withdraw
+		require(tokens[_token][msg.sender] >= _amount);
+
+
+		// Transfer tokens to user
+		Token(_token).transfer(msg.sender, _amount);
+
+		// Update user balance
+		tokens[_token][msg.sender] = tokens[_token][msg.sender] - _amount;
+
+		// Emit and event
+		emit Withdraw(_token, msg.sender, _amount, tokens[_token][msg.sender]);
+
 	}
 
 	// Check Balances
