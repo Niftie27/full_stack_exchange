d7f2bf8ae12ab2e13c5bba1af8b1632e952d9da9
tomas <tom.pazout@gmail.com>
2025-04-25 01:12:41 +0200

    25. Price chart

--- src/store/selectors.js
+++ src/store/selectors.js
@@ -1,16 +1,17 @@
 import { createSelector } from 'reselect'
-import { get, groupBy, reject } from 'lodash'
+import { get, groupBy, reject, maxBy, minBy } from 'lodash'
 import moment from 'moment'
 import { ethers } from 'ethers'
 
 const GREEN = '#25CE8F' 
 const RED = '#F45353'
 
+
 const tokens = state => get(state, 'tokens.contracts')
+
 const allOrders = state => get(state, 'exchange.allOrders.data', [])
 const cancelledOrders = state => get(state, 'exchange.cancelledOrders.data', [])
 const filledOrders = state => get(state, 'exchange.filledOrders.data', [])
-
 
 const openOrders = state => {
   const all = allOrders(state)
@@ -30,6 +31,8 @@ const openOrders = state => {
 const decorateOrder = (order, tokens) => {
   let token0Amount, token1Amount
 
+  // Note: DApp should be considered token0, mETH is considered token1
+  // Example: Giving mETH in exchange for DApp
   if (order.tokenGive === tokens[1].address) {
     token0Amount = order.amountGive // The amount of DApp we are giving
     token1Amount = order.amountGet // The amount of mETH we want...
@@ -38,16 +41,15 @@ const decorateOrder = (order, tokens) => {
     token1Amount = order.amountGive // The amount of mETH we are giving...
   }
 
-
-
+  // Calculate token price to 5 decimal places
   const precision = 100000
   let tokenPrice = (token1Amount / token0Amount)
   tokenPrice = Math.round(tokenPrice * precision) / precision
 
-  return({
+  return ({
   	...order,
-  	token0Amount: ethers.utils.formatUnits(token0Amount, "ether"),
   	token1Amount: ethers.utils.formatUnits(token1Amount, "ether"),
+  	token0Amount: ethers.utils.formatUnits(token0Amount, "ether"),
   	tokenPrice,
   	formattedTimestamp: moment.unix(order.timestamp).format('h:mm:ssa d MMM D')
   })
@@ -82,7 +84,8 @@ export const orderBookSelector = createSelector(
     }
 
     return orders
-})
+  }
+)
 
 const decorateOrderBookOrders = (orders, tokens) => {
 	return(
@@ -103,4 +106,68 @@ const decorateOrderBookOrder = (order, tokens) => {
 		orderTypeClass: (orderType === 'buy' ? GREEN : RED),
 		orderFillAction: (orderType === 'buy' ? 'sell' : 'buy')
 	})
+}
+
+
+// --------------------------------------------------------------------
+// PRICE CHART
+
+export const priceChartSelector = createSelector(
+  filledOrders,
+  tokens,
+  (orders, tokens) => {
+    if (!tokens[0] || !tokens[1]) { return }
+
+    // Filter orders by selected tokens
+    orders = orders.filter((o) => o.tokenGet === tokens[0].address || o.tokenGet === tokens[1].address)
+    orders = orders.filter((o) => o.tokenGive === tokens[0].address || o.tokenGive === tokens[1].address)
+
+    // Sort orders by date ascending to compare history
+    orders = orders.sort((a, b) => a.timestamp - b.timestamp)
+
+    // Decorate orders - add display attributes
+    orders = orders.map((o) => decorateOrder(o, tokens)) 
+
+    let secondLastOrder, lastOrder
+    [secondLastOrder, lastOrder] = orders.slice(orders.length - 2, orders.length)
+
+    const lastPrice = get(lastOrder, 'tokenPrice', 0)
+
+    const secondLastPrice = get(secondLastOrder, 'tokenPrice', 0)
+
+    return ({
+      lastPrice,
+      lastPriceChange: (lastPrice >= secondLastPrice ? '+' : '-'),
+      series: [{
+        data: buildGraphData(orders)
+      }]
+    })
+      
+  }
+)
+
+const buildGraphData = (orders) => {
+
+  orders = groupBy(orders, (o) => moment.unix(o.timestamp).startOf('hour').format())
+
+  // Get rach hour where data exists
+  const hours = Object.keys(orders)
+
+  const graphData = hours.map((hour) => {
+    // Fetch all orders from current hour
+    const group = orders[hour]
+
+    //Calculate price values: open, high, low, close
+    const open = group[0] // first order
+    const high = maxBy(group, 'tokenPrice') // high price
+    const low = minBy(group, 'tokenPrice') // low price
+    const close = group[group.length - 1] // last order
+
+    return({
+      x: new Date(hour),
+      y: [open.tokenPrice, high.tokenPrice, low.tokenPrice, close.tokenPrice]
+    })
+  })
+
+  return graphData
 }
